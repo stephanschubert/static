5c7bff08daddf4192f52168578bd4591e7cca43e
Stephan Schubert <stephan@frozencherry.de>
2011-09-27 21:08:51 +0200

    Slugify a page's title. The mongoid/slug plugin should be extracted into it's own gem soon.

--- app/models/static/page.rb
+++ app/models/static/page.rb
@@ -1,10 +1,12 @@
 module Static
   class Page
     include Mongoid::Document
+    include Mongoid::Slug
     include Mongoid::Timestamps
 
     field :title, type: String
     validates_presence_of :title
+    slug :title
 
     field :body,  type: String
     validates_presence_of :body
